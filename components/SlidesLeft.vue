216089796a3a9fe5ddf1580bf5be7ee84edb7afe
Erwin Rietveld <erietveld@leukeleu.nl>
2024-06-04 11:51:18 +0200

    Animation start template

--- components/SlidesLeft.vue
+++ components/SlidesLeft.vue
@@ -15,10 +15,25 @@ defineProps<{
   </SlideHolder>
   <SlideHolder :animationDirection="animationDirection">
     <SlideList v-if="currentSlide === 2">
-      <li>Test</li>
-      <li>Test</li>
-      <li>Test</li>
-      <li>Test</li>
+      <li><strong>01</strong> &nbsp; Drawing attention</li>
+      <li><strong>02</strong> &nbsp; Giving feedback</li>
+      <li><strong>03</strong> &nbsp; Communicate a change of state</li>
+      <li><strong>04</strong> &nbsp; Showing hierarchy of complex data</li>
+      <li><strong>05</strong> &nbsp; Learning how to interact</li>
+      <li><strong>06</strong> &nbsp; Because it's fun ✨</li>
+    </SlideList>
+  </SlideHolder>
+  <SlideHolder :animationDirection="animationDirection">
+    <SlideList v-if="currentSlide === 3">
+      <li>When the user must read a message</li>
+      <li>When the user must act immediately</li>
+      <li>Should not distract from page purpose</li>
+      <li>Often a dark pattern: 'fear of loss'</li>
+    </SlideList>
+  </SlideHolder>
+  <SlideHolder :animationDirection="animationDirection">
+    <SlideList v-if="currentSlide === 4">
+      <li></li>
     </SlideList>
   </SlideHolder>
 </template>
--- components/SlidesRight.vue
+++ components/SlidesRight.vue
@@ -15,11 +15,29 @@ defineProps<{
     />
   </SlideHolder>
   <SlideHolder :animationDirection="animationDirection">
-    <SlideList v-if="currentSlide === 2">
-      <li>Test</li>
-      <li>Test</li>
-      <li>Test</li>
-      <li>Test</li>
-    </SlideList>
+    <SlideTitle title="The roles of animation" v-if="currentSlide === 2" />
+  </SlideHolder>
+  <SlideHolder :animationDirection="animationDirection">
+    <SlideTitle title="Drawing attention" v-if="currentSlide === 3">
+      <template #animation>
+        <div class="animationelement-drawingattention">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+          >
+            <path
+              d="M16.971 0h-9.942l-7.029 7.029v9.941l7.029 7.03h9.941l7.03-7.029v-9.942l-7.029-7.029zm-5.971 5h2v10h-2v-10zm1 14.25c-.69 0-1.25-.56-1.25-1.25s.56-1.25 1.25-1.25 1.25.56 1.25 1.25-.56 1.25-1.25 1.25z"
+            />
+          </svg>
+          <div class="animationelement-line"></div>
+          <div class="animationelement-dot"><span></span></div>
+        </div>
+      </template>
+    </SlideTitle>
+  </SlideHolder>
+  <SlideHolder :animationDirection="animationDirection">
+    <SlideTitle title="Giving feedback" v-if="currentSlide === 4" />
   </SlideHolder>
 </template>
