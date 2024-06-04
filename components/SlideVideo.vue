f5f58591740bb71bab7bf019b8623264fc67b359
Erwin Rietveld <erietveld@leukeleu.nl>
2024-06-04 11:50:28 +0200

    Video slide template

---
+++ components/SlideVideo.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const videoPlayer = ref();
+
+defineProps<{
+  movie: string;
+}>();
+</script>
+
+<template>
+  <section>
+    <div
+      class="relative bg-leukeleudark shadow-leukeleu [&>video]:border-2 [&>video]:border-leukeleudarker"
+    >
+      <video width="360" height="290" controls loop ref="videoPlayer">
+        <source src="/videos/movie.mp4" type="video/mp4" />
+      </video>
+    </div>
+  </section>
+</template>
